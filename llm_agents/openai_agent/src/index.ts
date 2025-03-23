a8727631b91a7372f15536c27e871536b8cfc31b
hs4449889 <shida.haruhi@gmail.com>
2025-03-23 15:52:45 +0900

    破壊的変更: openAIAgentの名前を変更

--- llm_agents/openai_agent/src/index.ts
+++ llm_agents/openai_agent/src/index.ts
@@ -1,5 +1,6 @@
 import openAIAgent from "./openai_agent";
 import openAIImageAgent from "./openai_image_agent";
 
-export { openAIAgent };
+// エージェント名を変更して破壊的変更を加える
+export { openAIAgent as openAIAgentModified };
 export { openAIImageAgent };
