8b5031aee3289afecede4f28ae8c04a1c78a042b
hs4449889 <shida.haruhi@gmail.com>
2025-03-23 16:03:49 +0900

    fix modifed package

--- llm_agents/openai_agent/src/index.ts
+++ llm_agents/openai_agent/src/index.ts
@@ -1,6 +1,5 @@
 import openAIAgent from "./openai_agent";
 import openAIImageAgent from "./openai_image_agent";
 
-// エージェント名を変更して破壊的変更を加える
-export { openAIAgent as openAIAgentModified };
+export { openAIAgent };
 export { openAIImageAgent };
--- llm_agents/openai_agent/src/openai_agent.ts
+++ llm_agents/openai_agent/src/openai_agent.ts
@@ -205,7 +205,7 @@ export const openAIMockAgent: AgentFunction<OpenAIParams, OpenAIResult, OpenAIIn
   };
 };
 const openaiAgentInfo: AgentFunctionInfo = {
-  name: "openAIAgentModified", // 破壊的変更：エージェント名を変更
+  name: "openAIAgent",
   agent: openAIAgent,
   mock: openAIMockAgent,
   inputs: {
